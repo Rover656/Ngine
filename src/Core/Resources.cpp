f79e133f60e9df90dc1fc8382ec3de7ead83f0fc
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2019-06-04 13:51:07 +0100

    Dang, missed a quote.

--- src/Core/Resources.cpp
+++ src/Core/Resources.cpp
@@ -192,7 +192,7 @@ namespace NerdThings::Ngine::Core {
         std::vector<std::string> fntExts = {".ttf", ".otf", ".fnt"}; // TODO: Spritefont support
         std::vector<std::string> musExts = {"ogg", "flac", "mp3", "xm", "mod"};
         std::vector<std::string> sndExts = {"wav", "ogg", "flac", "mp3"};
-        std::vector<std::string> texExts = {"png", "bmp", "tga", "gif", "pic", "psd", "hdr", "dds", "pkm", "ktx", "pvr", "astc};
+        std::vector<std::string> texExts = {"png", "bmp", "tga", "gif", "pic", "psd", "hdr", "dds", "pkm", "ktx", "pvr", "astc"};
 
 
         // Load all files
