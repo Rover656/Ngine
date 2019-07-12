cdcbbb84c19a162bc7dee9d8e20fe3255cbe734f
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2019-07-12 17:15:14 +0100

    Add file headers

--- src/Graphics/Tileset.cpp
+++ src/Graphics/Tileset.cpp
@@ -1,3 +1,14 @@
+/**********************************************************************************************
+*
+*   Ngine - A (mainly) 2D game engine.
+*
+*   Copyright (C) 2019 NerdThings
+*
+*   LICENSE: Apache License 2.0
+*   View: https://github.com/NerdThings/Ngine/blob/master/LICENSE
+*
+**********************************************************************************************/
+
 #include "Tileset.h"
 
 #include "../Math/Rectangle.h"
