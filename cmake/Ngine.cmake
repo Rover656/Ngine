fcd04e3dd3dc6c9ccf55151db21507c9b37f8d20
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2019-10-13 16:16:02 +0100

    Completely overhaul cmake stuff

--- cmake/Ngine.cmake
+++ cmake/Ngine.cmake
@@ -1,53 +1,5 @@
 # General functions
 
-function(ngine_add_executable target)
-    # Add exe
-    add_executable(${target} WIN32 ${ARGN})
-
-    # UWP Specifics
-    if (${PLATFORM} MATCHES "UWP")
-        # Mark as WinRT Component
-        set_property(TARGET ${target} PROPERTY VS_WINRT_COMPONENT TRUE) # IDK if we really need this tho
-
-        # Link special libraries
-        target_link_libraries(${target} WindowsApp)
-
-        # No warnings
-        add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-    endif()
-endfunction()
-
-function(ngine_add_library target shared)
-    # Add lib
-    if (${shared})
-        add_library(${target} SHARED ${ARGN})
-    else()
-        add_library(${target} ${ARGN})
-    endif()
-
-    # UWP Specifics
-    if (${PLATFORM} MATCHES "UWP")
-        # Mark as WinRT Component
-        set_property(TARGET ${target} PROPERTY VS_WINRT_COMPONENT TRUE) # IDK if we really need this tho
-
-        # Link special libraries
-        target_link_libraries(${target} WindowsApp)
-
-        # No warnings
-        add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-    endif()
-endfunction()
-
-function(ngine_configure_uwp_executable manifest PROJECT_NAME PACKAGE_GUID PUBLISHER PUBLISHER_DISPLAY_NAME VERSION_MAJOR VERSION_MINOR VERSION_PATCH)
-    if (${PLATFORM} MATCHES "UWP")
-        # Configure manifest
-        configure_file(
-                ${manifest}
-                ${CMAKE_CURRENT_BINARY_DIR}/Package.appxmanifest
-                @ONLY)
-    endif()
-endfunction()
-
 # Check all config is okay
 function(ngine_check_config)
     if (${PLATFORM} MATCHES "Desktop")
@@ -64,90 +16,286 @@ function(ngine_check_config)
     endif()
 endfunction()
 
-# Link Ngine and schedule copying
-function(ngine_link_ngine target)
-    get_target_property(NGINE_SOURCE_DIR Ngine SOURCE_DIR)
+# TODO: Revisit this
+function(ngine_add_library target shared)
+    # Add lib
+    if (${shared})
+        add_library(${target} SHARED ${ARGN})
+    else()
+        add_library(${target} ${ARGN})
+    endif()
 
     # Link
-    target_include_directories(${target} PRIVATE ${NGINE_SOURCE_DIR})
-    target_link_libraries(${target} Ngine)
+    __ngine_link_ngine(${target})
 
-    # Copy shared lib
-    if (${BUILD_SHARED})
-        # Mark as shared
-        target_compile_definitions(${target} PRIVATE NGINE_SHARED=1)
-
-        # Copy, attached to Ngine target so that it will always happen
-        if(${PLATFORM} MATCHES "Desktop")
-            add_custom_command(TARGET ${target}
-                    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                    $<TARGET_FILE:Ngine>
-                    $<TARGET_FILE_DIR:${target}>)
-        endif()
-    endif()
-
-    if (${OPENGL_VERSION} MATCHES "3.3")
-        target_compile_definitions(${target} PRIVATE GRAPHICS_OPENGL33=1)
-    elseif (${OPENGL_VERSION} MATCHES "2.1")
-        target_compile_definitions(${target} PRIVATE GRAPHICS_OPENGL21=1)
-    elseif(${OPENGL_VERSION} MATCHES "ES2")
-        target_compile_definitions(${target} PRIVATE GRAPHICS_OPENGLES2=1)
-
-        if(${PLATFORM} MATCHES "Desktop")
-            add_custom_command(TARGET ${target}
-                    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                    ${NGINE_SOURCE_DIR}/../third-party/ANGLE/lib/x86/libEGL.dll
-                    $<TARGET_FILE_DIR:${target}>
-                    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                    ${NGINE_SOURCE_DIR}/../third-party/ANGLE/lib/x86/libGLESv2.dll
-                    $<TARGET_FILE_DIR:${target}>
-                    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                    ${NGINE_SOURCE_DIR}/../third-party/ANGLE/lib/x86/d3dcompiler_47.dll
-                    $<TARGET_FILE_DIR:${target}>)
-        endif()
-    endif()
-
-    if (${PLATFORM} MATCHES "Desktop")
-        target_compile_definitions(${target} PRIVATE PLATFORM_DESKTOP=1)
-    elseif(${PLATFORM} MATCHES "UWP")
-        target_compile_definitions(${target} PRIVATE PLATFORM_UWP=1)
-    endif()
+    # UWP additions
+    __ngine_post_uwp_additions(${target})
 endfunction()
 
-# Content Management
+function(ngine_add_game)
+    # Get parameters
+    set (options)
+    set (oneValueArgs
+            NAME # Game name
+            SHORT_NAME # Shortened game name
+            TARGET_NAME # Target name
 
-function(ngine_add_content target root_dir content_dir)
+            CONTENT_DIR # Content directory
+
+            VERSION_MAJOR
+            VERSION_MINOR
+            VERSION_PATCH
+
+            UWP_PACKAGE_NAME # Ex 00000DeveloperName.AppName
+            UWP_PACKAGE_GUID
+            UWP_PUBLISHER # Publisher ID or name if unknown, CN=...
+            UWP_PUBLISHER_DISPLAY_NAME # Publisher display name, E.g. NerdThings
+            UWP_DESCRIPTION # Game description, otherwise it will be set to short name
+
+            # UWP Asset names
+            UWP_STORE_LOGO_NAME # StoreLogo.png
+            UWP_SPLASH_SCREEN_NAME # SplashScreen.png
+            UWP_310x150_WIDE_NAME # Wide310x150Logo.png
+            UWP_44x44_SQUARE_NAME # Square44x44Logo.png
+            UWP_150x150_SQUARE_NAME # Square150x150Logo.png
+
+            # UWP Store association resources
+            UWP_PACKAGE_STORE_ASSOCIATION_PATH # If set, included in project root
+            UWP_PACKAGE_STORE_KEY # If set, included in project root
+            )
+    set (multiValueArgs
+            SOURCE_FILES
+            RESOURCE_FILES)
+    cmake_parse_arguments(GAME "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    # TODO: UWP Capabilities
+
+    # Argument validation
+    if (NOT GAME_NAME OR "${GAME_NAME}" STREQUAL "")
+        set(GAME_NAME "Made with Ngine")
+    endif()
+
+    if (NOT GAME_SHORT_NAME OR "${GAME_SHORT_NAME}" STREQUAL "")
+        set(GAME_SHORT_NAME ${GAME_NAME})
+    endif()
+
+    if (NOT GAME_TARGET_NAME OR "${GAME_TARGET_NAME}" STREQUAL "")
+        message(FATAL_ERROR "[Ngine] Target name cannot be blank")
+    endif()
+
+    if (NOT GAME_CONTENT_DIR OR "${GAME_CONTENT_DIR}" STREQUAL "")
+        set(GAME_CONTENT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/content")
+    endif()
+
+    if (NOT GAME_VERSION_MAJOR OR "${GAME_VERSION_MAJOR}" STREQUAL "")
+        set(GAME_VERSION_MAJOR "1")
+    endif()
+
+    if (NOT GAME_VERSION_MINOR OR "${GAME_VERSION_MINOR}" STREQUAL "")
+        set(GAME_VERSION_MINOR "0")
+    endif()
+
+    if (NOT GAME_VERSION_PATCH OR "${GAME_VERSION_PATCH}" STREQUAL "")
+        set(GAME_VERSION_PATCH "0")
+    endif()
+
+    if (NOT GAME_UWP_PACKAGE_NAME OR "${GAME_UWP_PACKAGE_NAME}" STREQUAL "")
+        set(GAME_UWP_PACKAGE_NAME ${GAME_NAME})
+    endif()
+
+    if (NOT GAME_UWP_PACKAGE_GUID OR "${GAME_UWP_PACKAGE_GUID}" STREQUAL "")
+        if (${PLATFORM} MATCHES "UWP")
+            message(FATAL_ERROR "[Ngine] Must set the UWP package GUID")
+        else()
+            message("[Ngine] You must set the UWP package GUID if you wish to target UWP.")
+        endif()
+    endif()
+
+    if (NOT GAME_UWP_PUBLISHER_DISPLAY_NAME OR "${GAME_UWP_PUBLISHER_DISPLAY_NAME}" STREQUAL "")
+        if (${PLATFORM} MATCHES "UWP")
+            message(FATAL_ERROR "[Ngine] Must set the UWP publisher display name")
+        else()
+            message("[Ngine] You must set the UWP publisher display name if you wish to target UWP.")
+        endif()
+    endif()
+
+    if (NOT GAME_UWP_PUBLISHER OR "${GAME_UWP_PUBLISHER}" STREQUAL "")
+        if (${PLATFORM} MATCHES "UWP")
+            message(WARNING "[Ngine] Setting publisher parameter to publisher display name.")
+        endif()
+        set(GAME_UWP_PUBLISHER ${GAME_UWP_PUBLISHER_DISPLAY_NAME})
+    endif()
+
+    if (NOT GAME_UWP_DESCRIPTION OR "${GAME_UWP_DESCRIPTION}" STREQUAL "")
+        set(GAME_UWP_DESCRIPTION ${GAME_SHORT_NAME})
+    endif()
+
+    if (NOT GAME_UWP_STORE_LOGO_NAME OR "${GAME_UWP_STORE_LOGO_NAME}" STREQUAL "")
+        set(GAME_UWP_STORE_LOGO_NAME "StoreLogo.png")
+    endif()
+
+    if (NOT GAME_UWP_SPLASH_SCREEN_NAME OR "${GAME_UWP_SPLASH_SCREEN_NAME}" STREQUAL "")
+        set(GAME_UWP_SPLASH_SCREEN_NAME "SplashScreen.png")
+    endif()
+
+    if (NOT GAME_UWP_310x150_WIDE_NAME OR "${GAME_UWP_310x150_WIDE_NAME}" STREQUAL "")
+        set(GAME_UWP_310x150_WIDE_NAME "Wide310x150Logo.png")
+    endif()
+
+    if (NOT GAME_UWP_44x44_SQUARE_NAME OR "${GAME_UWP_44x44_SQUARE_NAME}" STREQUAL "")
+        set(GAME_UWP_44x44_SQUARE_NAME "Square44x44Logo.png")
+    endif()
+
+    if (NOT GAME_UWP_150x150_SQUARE_NAME OR "${GAME_UWP_150x150_SQUARE_NAME}" STREQUAL "")
+        set(GAME_UWP_150x150_SQUARE_NAME "Square150x150Logo.png")
+    endif()
+
+    # Get content files
+    file(GLOB_RECURSE GAME_CONTENT_FILES "${GAME_CONTENT_DIR}/*")
+
+    if(${PLATFORM} MATCHES "UWP")
+        # Setup UWP manifest
+        if (${PLATFORM} MATCHES "UWP")
+            # Configure manifest
+            configure_file(
+                    ${CMAKE_MODULE_PATH}/UWP/Package.appxmanifest.in
+                    ${CMAKE_CURRENT_BINARY_DIR}/Package.appxmanifest
+                    @ONLY)
+
+            set(GAME_RESOURCE_FILES ${GAME_RESOURCE_FILES} ${CMAKE_CURRENT_BINARY_DIR}/Package.appxmanifest)
+        endif()
+
+        # UWP add store association stuff if present
+        if (NOT GAME_UWP_PACKAGE_STORE_ASSOCIATION_PATH OR "${GAME_UWP_PACKAGE_STORE_ASSOCIATION_PATH}" STREQUAL "")
+            set (GAME_ADDITIONAL_FILES ${GAME_ADDITIONAL_FILES} ${GAME_UWP_PACKAGE_STORE_ASSOCIATION_PATH})
+        endif()
+
+        if (NOT GAME_UWP_PACKAGE_STORE_KEY OR "${GAME_UWP_PACKAGE_STORE_KEY}" STREQUAL "")
+            set (GAME_ADDITIONAL_FILES ${GAME_ADDITIONAL_FILES} ${GAME_UWP_PACKAGE_STORE_KEY})
+        endif()
+    endif()
+
+    # Configure game
+    add_executable(${GAME_TARGET_NAME} WIN32 ${GAME_SOURCE_FILES} ${GAME_CONTENT_FILES} ${GAME_RESOURCE_FILES} ${GAME_ADDITIONAL_FILES})
+
+    # Visual Studio Source Groups
+    if(MSVC)
+        source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} PREFIX "\\Source Files" FILES ${GAME_SOURCE_FILES})
+        source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}/content PREFIX "\\Content" FILES ${GAME_CONTENT_FILES})
+        source_group("Assets" FILES ${GAME_RESOURCE_FILES})
+    endif()
+
+    # Post UWP additions
+    __ngine_post_uwp_additions(${GAME_TARGET_NAME})
+
+    # Link Ngine
+    __ngine_link_ngine(${GAME_TARGET_NAME})
+
+    # Get content dir name
+    get_filename_component(CONTENT_DIR_NAME ${GAME_CONTENT_DIR} NAME)
+
+    # Include content
     if (${PLATFORM} MATCHES "Desktop")
-        add_custom_command(TARGET ${target} PRE_BUILD
+        add_custom_command(TARGET ${GAME_TARGET_NAME} PRE_BUILD
                 COMMAND ${CMAKE_COMMAND} -E copy_directory
-                ${root_dir}/${content_dir}
-                $<TARGET_FILE_DIR:${target}>/${content_dir})
+                ${GAME_CONTENT_DIR}
+                $<TARGET_FILE_DIR:${GAME_TARGET_NAME}>/${CONTENT_DIR_NAME})
     elseif(${PLATFORM} MATCHES "UWP")
-        # Get all files
-        file(GLOB_RECURSE content_files "${root_dir}/${content_dir}/*")
-
         # Mark for deploy
-        set_property(SOURCE ${content_files} PROPERTY VS_DEPLOYMENT_CONTENT 1)
+        set_property(SOURCE ${GAME_CONTENT_FILES} PROPERTY VS_DEPLOYMENT_CONTENT 1)
 
         # Setup deployment directories
-        foreach(CONTENT_FILE ${content_files})
+        foreach(CONTENT_FILE ${GAME_CONTENT_FILES})
             # Get filename
             get_filename_component(CONTENT_FILENAME ${CONTENT_FILE} NAME)
 
-            # Remove initial path
-            string(REPLACE "${root_dir}/" "" CONTENT_FILE_DEPLOYLOC ${CONTENT_FILE})
+            # Remove path
+            string(REPLACE "${GAME_CONTENT_DIR}/" "" CONTENT_FILE_DEPLOYLOC ${CONTENT_FILE})
 
             # Remove filename
-            string(REPLACE "/${CONTENT_FILENAME}" "" CONTENT_FILE_DEPLOYLOC ${CONTENT_FILE_DEPLOYLOC})
+            string(REPLACE "${CONTENT_FILENAME}" "" CONTENT_FILE_DEPLOYLOC ${CONTENT_FILE_DEPLOYLOC})
+
+            # Add content dir name to start
+            set(CONTENT_FILE_DEPLOYLOC ${CONTENT_DIR_NAME}/${CONTENT_FILE_DEPLOYLOC})
+
+            # Remove trailing slash
+            string(REGEX REPLACE "\\/$" "" CONTENT_FILE_DEPLOYLOC ${CONTENT_FILE_DEPLOYLOC})
 
             set_property(SOURCE ${CONTENT_FILE} PROPERTY VS_DEPLOYMENT_LOCATION ${CONTENT_FILE_DEPLOYLOC})
         endforeach()
 
-        # Resource files
-        if (ARGN)
-            # Mark as deployable
-            set_property(SOURCE ${RESOURCE_FILES} PROPERTY VS_DEPLOYMENT_CONTENT 1)
-            set_property(SOURCE ${RESOURCE_FILES} PROPERTY VS_DEPLOYMENT_LOCATION "Assets")
+        # Mark as deployable
+        foreach(RESOURCE_FILE ${RESOURCE_FILES})
+            # TODO: Support subdirectories
+            set_property(SOURCE ${RESOURCE_FILE} PROPERTY VS_DEPLOYMENT_CONTENT 1)
+            set_property(SOURCE ${RESOURCE_FILE} PROPERTY VS_DEPLOYMENT_LOCATION "Assets")
+        endforeach()
+    endif()
+endfunction()
+
+### Internal Functions
+
+function(__ngine_post_uwp_additions GAME_TARGET_NAME)
+    # UWP Specifics
+    if (${PLATFORM} MATCHES "UWP")
+        # Mark as WinRT Component
+        set_property(TARGET ${GAME_TARGET_NAME} PROPERTY VS_WINRT_COMPONENT TRUE) # IDK if we really need this tho
+
+        # Link special libraries
+        target_link_libraries(${GAME_TARGET_NAME} WindowsApp)
+
+        # No warnings
+        add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+    endif()
+endfunction()
+
+# Link Ngine and schedule copying
+function(__ngine_link_ngine GAME_TARGET_NAME)
+    # Link Ngine
+    get_target_property(NGINE_SOURCE_DIR Ngine SOURCE_DIR)
+
+    # Link
+    target_include_directories(${GAME_TARGET_NAME} PRIVATE ${NGINE_SOURCE_DIR})
+    target_link_libraries(${GAME_TARGET_NAME} Ngine)
+
+    # Copy shared lib
+    if (${BUILD_SHARED})
+        # Mark as shared
+        target_compile_definitions(${GAME_TARGET_NAME} PRIVATE NGINE_SHARED=1)
+
+        # Copy, attached to Ngine target so that it will always happen
+        if(${PLATFORM} MATCHES "Desktop")
+            add_custom_command(TARGET ${GAME_TARGET_NAME}
+                    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                    $<TARGET_FILE:Ngine>
+                    $<TARGET_FILE_DIR:${GAME_TARGET_NAME}>)
         endif()
+    endif()
+
+    if (${OPENGL_VERSION} MATCHES "3.3")
+        target_compile_definitions(${GAME_TARGET_NAME} PRIVATE GRAPHICS_OPENGL33=1)
+    elseif (${OPENGL_VERSION} MATCHES "2.1")
+        target_compile_definitions(${GAME_TARGET_NAME} PRIVATE GRAPHICS_OPENGL21=1)
+    elseif(${OPENGL_VERSION} MATCHES "ES2")
+        target_compile_definitions(${GAME_TARGET_NAME} PRIVATE GRAPHICS_OPENGLES2=1)
+
+        if(${PLATFORM} MATCHES "Desktop")
+            add_custom_command(TARGET ${GAME_TARGET_NAME}
+                    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                    ${NGINE_SOURCE_DIR}/../third-party/ANGLE/lib/x86/libEGL.dll
+                    $<TARGET_FILE_DIR:${GAME_TARGET_NAME}>
+                    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                    ${NGINE_SOURCE_DIR}/../third-party/ANGLE/lib/x86/libGLESv2.dll
+                    $<TARGET_FILE_DIR:${GAME_TARGET_NAME}>
+                    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                    ${NGINE_SOURCE_DIR}/../third-party/ANGLE/lib/x86/d3dcompiler_47.dll
+                    $<TARGET_FILE_DIR:${GAME_TARGET_NAME}>)
+        endif()
+    endif()
+
+    if (${PLATFORM} MATCHES "Desktop")
+        target_compile_definitions(${GAME_TARGET_NAME} PRIVATE PLATFORM_DESKTOP=1)
+    elseif(${PLATFORM} MATCHES "UWP")
+        target_compile_definitions(${GAME_TARGET_NAME} PRIVATE PLATFORM_UWP=1)
     endif()
 endfunction()
