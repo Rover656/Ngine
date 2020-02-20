28365b67d964dd69dbe1b21fc19772d6185407d4
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2020-02-20 12:38:46 +0000

    Update DeployToFtp.ps1

--- DeployToFtp.ps1
+++ DeployToFtp.ps1
@@ -1,7 +1,13 @@
-Write-Host "Starting deploy"
-$envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@ftp.nerdthings.dev/docs/test""}}}}' -f $env:ftp_user, $env:ftp_password
-creep -e $envConf -d '{""source"": ""hash""}' -b build/docs/docs/html -y
-if ($lastExitCode -ne 0)
-{
-    exit -1
+if ($env:APPVEYOR_REPO_NAME -ne "NerdThings/Ngine") {
+    exit 0
+}
+
+if ($env:APPVEYOR_REPO_BRANCH -e "master" -or $env:APPVEYOR_REPO_BRANCH -e "develop") {
+    Write-Host "Starting deploy..."
+    $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@ftp.nerdthings.dev/{1}""}}}}' -f $env:ftp_user, $env:ftp_password, $env:APPVEYOR_REPO_BRANCH
+    creep -e $envConf -d '{""source"": ""hash""}' -b build/docs/docs/html -y
+    if ($lastExitCode -ne 0)
+    {
+        exit -1
+     }
 }
