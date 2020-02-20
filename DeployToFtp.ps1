434adfe30294597ff92d2ac954c8c2ed2e37c735
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2020-02-20 12:43:29 +0000

    Update DeployToFtp.ps1

--- DeployToFtp.ps1
+++ DeployToFtp.ps1
@@ -1,8 +1,8 @@
 if ($env:APPVEYOR_REPO_NAME -ne "NerdThings/Ngine") {
-    exit 0
+exit 0
 }
 
-if ($env:APPVEYOR_REPO_BRANCH -e "master" -or $env:APPVEYOR_REPO_BRANCH -e "develop") {
+if ($env:APPVEYOR_REPO_BRANCH -eq "master" -or $env:APPVEYOR_REPO_BRANCH -eq "develop") {
     Write-Host "Starting deploy..."
     $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@ftp.nerdthings.dev/{1}""}}}}' -f $env:ftp_user, $env:ftp_password, $env:APPVEYOR_REPO_BRANCH
     creep -e $envConf -d '{""source"": ""hash""}' -b build/docs/docs/html -y
