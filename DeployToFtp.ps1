c43bf9e9f2d426f71c23d9c13c86b19f2a46c225
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2020-02-20 12:48:25 +0000

    oops.

--- DeployToFtp.ps1
+++ DeployToFtp.ps1
@@ -4,7 +4,7 @@ exit 0
 
 if ($env:APPVEYOR_REPO_BRANCH -eq "master" -or $env:APPVEYOR_REPO_BRANCH -eq "develop") {
     Write-Host "Starting deploy..."
-    $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@ftp.nerdthings.dev/{1}""}}}}' -f $env:ftp_user, $env:ftp_password, $env:APPVEYOR_REPO_BRANCH
+    $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@ftp.nerdthings.dev/{2}""}}}}' -f $env:ftp_user, $env:ftp_password, $env:APPVEYOR_REPO_BRANCH
     creep -e $envConf -d '{""source"": ""hash""}' -b build/docs/docs/html -y
     if ($lastExitCode -ne 0)
     {
