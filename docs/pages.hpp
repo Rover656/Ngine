c18f431e4621b3e61c807963f10ed1a3ecce63bb
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2020-02-28 09:50:52 +0000

    [Docs] Remove old mainpage.

--- docs/pages.hpp
+++ docs/pages.hpp
@@ -3,15 +3,6 @@
 // ---------------------------------------------------------------------------------
 
 /**
- * @mainpage Ngine API Documentation
- *
- * @section intro_sec Introduction
- * Ngine is a 2D game engine written in C++.
- *
- * @section installation Installation
- * NerdThings does not currently provide binaries for Ngine, you must build it from source instead.
- * Build instructions are available on the @ref platform_support "Platform Support" page
- * 
  * @page helpful_macros Helpful Macros
  * @tableofcontents
  *
