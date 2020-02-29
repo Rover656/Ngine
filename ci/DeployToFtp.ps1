c273cf5583b3e44db718447f975d19a46861dc71
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2020-02-29 19:53:42 +0000

    [CI] Fix FTP deploy.

--- ci/DeployToFtp.ps1
+++ ci/DeployToFtp.ps1
@@ -5,7 +5,7 @@ exit 0
 if ($env:APPVEYOR_REPO_BRANCH -eq "master" -or $env:APPVEYOR_REPO_BRANCH -eq "develop") {
     Write-Host "Starting deploy..."
     $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@ftp.nerdthings.dev/{2}""}}}}' -f $env:ftp_user, $env:ftp_password, $env:APPVEYOR_REPO_BRANCH
-    creep -e $envConf -d '{""source"": ""hash""}' -b build/docs/docs/html -y
+    creep -e $envConf -d '{""source"": ""hash""}' -b ../build/docs/docs/html -y
     if ($lastExitCode -ne 0)
     {
         exit -1
